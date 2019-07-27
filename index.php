559dedc9366043501969485fae198eb24d766ad6
ayoub gharbi <ayoub97ma@gmail.com>
2019-07-27 23:16:01 +0100

    Foreach loop in php

--- index.php
+++ index.php
@@ -6,30 +6,16 @@
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
 <?php
-    $dayofweek = date("w");
-    switch ($dayofweek) {
-        case 1:
-            echo "It is Monday!";
-        break;
-        case 2:
-            echo "It is Tuesday!";
-        break;
-        case 3:
-            echo "It is Wednesday!";
-        break;
-        case 4:
-            echo "It is Thursday!";
-        break;
-        case 5:
-            echo "It is Friday!";
-        break;
-        case 6:
-            echo "<p>It is Saturday!</p>";
-        break;
-        case 7:
-            echo "It is Sunday!";
-        break;
+    //Loop
+    //While Loop
+    //Do while Loop
+    //For Loop
+    //Foreach Loop
+    $array = array("Daniel", "Jane", "Jacob", "John", "Mariane");
+    foreach ($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>"; 
     }
 ?>
 </body>
