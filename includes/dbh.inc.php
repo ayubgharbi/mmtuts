bc0ed4cefbf31b79ef2ed619344eda775c169594
ayoub gharbi <ayoub97ma@gmail.com>
2019-07-30 19:41:30 +0100

    Protect database against SQL injection using MySQLi

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,13 +1,14 @@
-<?php
-    include_once 'dbh.inc.php'; 
-    
-    $first = mysqli_real_escape_string($conn, $_POST['first']);
-    $last = mysqli_real_escape_string($conn, $_POST['last']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $uid = mysqli_real_escape_string($conn, $_POST['uid']);
-    $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+<?php 
 
-    $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
-    mysqli_query($conn, $sql);
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
 
-    header("Location: ../index.php?signup=success");
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+?>
+
+
+
