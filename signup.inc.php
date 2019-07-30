f0ef5deed8d04ddac5ad2678b36f285ddf96b936
ayoub gharbi <ayoub97ma@gmail.com>
2019-07-30 18:47:29 +0100

    Protect database against SQL injection using MySQLi

--- signup.inc.php
+++ signup.inc.php
@@ -9,6 +9,5 @@
 
     $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
     mysqli_query($conn, $sql);
-    
 
     header("Location: ../index.php?signup=success");
