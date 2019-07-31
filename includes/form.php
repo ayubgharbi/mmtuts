a69a974a3f907672d73605fc4ee1641992739ace
ayoub gharbi <ayoub97ma@gmail.com>
2019-07-31 02:56:01 +0100

    Prepared Statements

---
+++ includes/form.php
@@ -0,0 +1,13 @@
+<form action="includes/signup.inc.php" method="POST">
+    <input type="text" name="first" placeholder="Firstname">
+    <br>
+    <input type="text" name="last" placeholder="Lasttname">
+    <br>
+    <input type="text" name="email" placeholder="Email">
+    <br>
+    <input type="text" name="uid" placeholder="Username">
+    <br>
+    <input type="password" name="pwd" placeholder="Password">
+    <br>
+    <button type="submit" name="submit">Sign up</button>
+</form>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    include_once 'includes/dbh.inc.php'; 
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,19 +9,35 @@
 </head>
 <body>
 
-<form action="includes/signup.inc.php" method="POST">
-    <input type="text" name="first" placeholder="Firstname">
-    <br>
-    <input type="text" name="last" placeholder="Lasttname">
-    <br>
-    <input type="text" name="email" placeholder="Email">
-    <br>
-    <input type="text" name="uid" placeholder="Username">
-    <br>
-    <input type="password" name="pwd" placeholder="Password">
-    <br>
-    <button type="submit" name="submit">Sign up</button>
-</form>
+    
+
+<?php
+    include 'includes/form.php';
+    
+    $data = "Admin";
+    //Created a template
+    $sql = "SELECT * FROM users WHERE user_uid=?;";
+    //Create a prepared statement
+    $stmt = mysqli_stmt_init($conn);
+    //Prepaare the prepared statement
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "SQL statement failed";    
+    } else {
+        //Bind parameters to the placeholder
+        mysqli_stmt_bind_param($stmt, "s", $data);
+        //Run parameters inside database
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['user_uid'] . "<br>";
+        }
+    }
+    
+
+        
+          
+?>
 
 
 
